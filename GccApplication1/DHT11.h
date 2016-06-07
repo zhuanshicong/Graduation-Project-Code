b126352753e0346ea9907d3ca908dbbdf5578824
zhuanshicong <xiaozhucong@gmail.com>
2016-06-08 01:48:26 +0800

    Revert "Graduation-Project-Code"
    This reverts commit 74cebc97424318fcedc0a42fd9a52fd17ebd6ffb.

--- GccApplication1/DHT11.h
+++
@@ -1,8 +0,0 @@
-#ifndef DHT11_H
-#define DHT11_H
-#define DHT_DATA_I DDRA &= ~(1<<PA0)
-#define DHT_DATA_O DDRA |=  (1<<PA0)
-#define DHT_L      PORTA &= ~(1<<PA0)
-#define DHT_H      PORTA |=  (1<<PA0)
-#define CHECK_DHT_H         (PINA&0x01)
-#endif
